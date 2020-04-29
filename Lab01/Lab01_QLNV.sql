5d580e97243ae1656950abac0536a46565c3c084
DalatCoder <hieuntctk42@gmail.com>
2020-04-29 11:42:03 +0700

    Hoan thanh truy van long Lab01 Quan ly nhan vine

--- Lab01/Lab01_QLNV.sql
+++ Lab01/Lab01_QLNV.sql
@@ -9,12 +9,11 @@
 
 --Tạo cơ sở dữ liệu Quản lý nhân viên
 CREATE DATABASE Lab01_QuanLyNhanVien
-
--- Go để bắt đầu 1 lệnh
-go
+GO
 
 -- Sử dụng cơ sở dữ liệu Quản lý nhân viên
-use Lab01_QuanLyNhanVien
+USE Lab01_QuanLyNhanVien
+GO
 
 -- Tạo các bảng tương ứng trong cơ sở dữ liệu
 -- Bảng chi nhánh, với MSCN là khóa chính với 2 kí tự char, Tên chi nhánh lưu Unicode với chiều dài 30 kí tự, không được để trống và không được trùng tên
@@ -111,46 +110,135 @@ select * from NhanVien
 select * from KyNang
 select * from NhanVienKyNang
 
--- Truy vấn dữ liệu --
--- Q1: Hiển thị MSNV, HoTen (Ho + Ten as HoTen), số năm làm việc (SoNamLamViec)
+-- 1. Truy vấn lựa chọn trên nhiều bảng
+-- a) Hiển thị MSNV, HoTen (Ho + Ten as HoTen), số năm làm việc (SoNamLamViec).
 SELECT MANV, Ho + ' ' + Ten AS HoTen, YEAR(GETDATE()) - YEAR(NgayVaoLam) AS SoNamLamViec
 FROM NhanVien
 
--- Q2: Liệt kê các thông tin về nhân viên: HoTen, NgaySinh, NgayVaoLam, TenCN (sắp xếp theo tên chi nhánh).
-SELECT Ho + ' ' + Ten AS HoTen, Ngaysinh, NgayVaoLam, TenCN
+-- b) Liệt kê các thông tin về nhân viên: HoTen, NgaySinh, NgayVaoLam, TenCN (sắp xếp theo tên chi nhánh).
+SELECT Ho + ' ' + Ten AS HoTen, CONVERT(char(10), NgaySinh, 103) AS NgaySinh, CONVERT(char(10), NgayVaoLam, 103) AS NgayVaoLam, TenCN
 FROM NhanVien, ChiNhanh
 WHERE NhanVien.MSCN = ChiNhanh.MSCN
-ORDER BY ChiNhanh.TenCN
+ORDER BY TenCN, Ten
 
--- Q3: Liệt kê các nhân viên (HoTen, TenKN, MucDo) của những nhân viên biết sử dụng ‘Word’.
-SELECT Ho + ' ' + Ten AS HoTen, TenKN, MucDo
+
+-- c) Liệt kê các nhân viên (HoTen, TenKN, MucDo) của những nhân viên biết sử dụng ‘Word’.
+SELECT Ho + ' ' + Ten AS HoTen, TenKN, MucDo 
 FROM NhanVien, KyNang, NhanVienKyNang
-WHERE NhanVien.MANV = NhanVienKyNang.MANV and KyNang.MSKN = NhanVienKyNang.MSKN and TenKN = N'Word'
+WHERE  
+	NhanVien.MANV = NhanVienKyNang.MANV AND KyNang.MSKN = NhanVienKyNang.MSKN AND
+	KyNang.TenKN = N'Word'
+ORDER BY MucDo
 
--- Q4: Liệt kê các kỹ năng (TenKN, MucDo) mà nhân viên ‘Lê Anh Tuấn’ biết sử dụng.
+
+-- d) Liệt kê các kỹ năng (TenKN, MucDo) mà nhân viên ‘Lê Anh Tuấn’ biết sử dụng.
 SELECT TenKN, MucDo
 FROM NhanVien, KyNang, NhanVienKyNang
-WHERE 
-	NhanVien.Ho = N'Lê Anh' and NhanVien.Ten = N'Tuấn' and 
-	NhanVien.MANV = NhanVienKyNang.MANV and KyNang.MSKN = NhanVienKyNang.MSKN
+WHERE
+	NhanVien.MANV = NhanVienKyNang.MANV AND KyNang.MSKN = NhanVienKyNang.MSKN AND
+	NhanVien.Ho = N'Lê Anh' AND NhanVien.Ten = N'Tuấn'
+ORDER BY MucDo
+
+-- 2. Truy vấn lồng
+-- a) Liệt kê MANV, HoTen, MSCN, TenCN của các nhân viên có mức độ thành thạo về ‘Excel’ cao nhất trong công ty .
+SELECT NhanVien.MANV, Ho, Ten, ChiNhanh.MSCN, TenCN, MucDo
+FROM NhanVien, NhanVienKyNang, KyNang, ChiNhanh
+WHERE
+	NhanVien.MSCN = ChiNhanh.MSCN AND NhanVien.MANV = NhanVienKyNang.MANV AND KyNang.MSKN = NhanVienKyNang.MSKN AND
+	KyNang.TenKN = N'Excel'
+GROUP BY NhanVien.MANV, Ho, Ten, ChiNhanh.MSCN, TenCN, MucDo
+HAVING MucDo >= ALL(
+		SELECT MucDo
+		FROM KyNang, NhanVienKyNang
+		WHERE KyNang.MSKN = NhanVienKyNang.MSKN AND KyNang.TenKN = N'Excel'
+	)
+	
+
+-- b) Liệt kê MANV, HoTen, TenCN của các nhân viên vừa biết ‘Word’ vừa biết ‘Excel’ (dùng truy vấn lồng).
+-- LIỆT KÊ NHÂN VIÊN BIẾT WORD
+SELECT NhanVien.MANV, Ho, Ten, ChiNhanh.MSCN, TenCN
+FROM NhanVien, NhanVienKyNang, KyNang, ChiNhanh
+WHERE
+	NhanVien.MSCN = ChiNhanh.MSCN AND NhanVien.MANV = NhanVienKyNang.MANV AND KyNang.MSKN = NhanVienKyNang.MSKN AND
+	KyNang.TenKN = N'Word' AND
+	NhanVien.MANV IN (
+		-- LIỆT KÊ NHÂN VIÊN BIẾT EXCEL 
+		SELECT NhanVien.MANV
+		FROM NhanVien, NhanVienKyNang, KyNang
+		WHERE
+			NhanVien.MANV = NhanVienKyNang.MANV AND KyNang.MSKN = NhanVienKyNang.MSKN AND
+			KyNang.TenKN = N'Excel'
+		)
+
+-- c) Với từng kỹ năng, hãy liệt kê các thông tin (MANV, HoTen, TenCN, TenKN, MucDo) của những nhân viên thành thạo kỹ năng đó nhất.
 
 
--- Q5: Cho biết số người dùng của từng kĩ năng gồm MSKN, TenKN, SoND
-SELECT KyNang.MSKN, TenKN, COUNT(NhanVienKyNang.MANV) as SoND
-FROM KyNang, NhanVienKyNang
-WHERE KyNang.MSKN = NhanVienKyNang.MSKN
-GROUP BY KyNang.MSKN, TenKN
+-- d) Liệt kê các chi nhánh (MSCN, TenCN) mà mọi nhân viên trong chi nhánh đó đều biết ‘Word’.
 
--- Q6: Cho biết những kĩ năng có trên 3 người dùng
-SELECT KyNang.MSKN, TenKN, COUNT(NhanVienKyNang.MANV) as SoND
-FROM KyNang, NhanVienKyNang
-WHERE KyNang.MSKN = NhanVienKyNang.MSKN
-GROUP BY KyNang.MSKN, TenKN
-HAVING COUNT(NhanVienKyNang.MANV) > 3
 
--- Q7: Liệt kê nhân viên biết từ 3 kỹ năng trở lên
-SELECT NhanVien.MANV, Ho, Ten, Ngaysinh, COUNT(MSKN) AS SoKN
+
+-- 3. Truy vấn gom nhóm dữ liệu.
+-- a) Với mỗi chi nhánh, hãy cho biết các thông tin sau TenCN, SoNV (số nhân viên của chi nhánh đó).
+SELECT ChiNhanh.TenCN, COUNT(NhanVien.MANV) AS SoNV
+FROM ChiNhanh, NhanVien
+WHERE ChiNhanh.MSCN = NhanVien.MSCN
+GROUP BY ChiNhanh.MSCN, ChiNhanh.TenCN
+ORDER BY TenCN
+
+-- b) Với mỗi kỹ năng, hãy cho biết TenKN, SoNguoiDung (số nhân viên biết sử dụng kỹ năng đó).
+SELECT TenCN, TenKN, COUNT(NhanVien.MANV) AS SoNguoiDung
+FROM ChiNhanh, NhanVien, NhanVienKyNang, KyNang
+WHERE ChiNhanh.MSCN = NhanVien.MSCN AND NhanVien.MANV = NhanVienKyNang.MANV AND KyNang.MSKN = NhanVienKyNang.MSKN
+GROUP BY TenCN, TenKN
+ORDER BY TenCN
+
+-- c) Cho biết TenKN có từ 3 nhân viên trong công ty sử dụng trở lên.
+SELECT TenKN
+FROM NhanVienKyNang, KyNang
+WHERE NhanVienKyNang.MSKN = KyNang.MSKN
+GROUP BY TenKN
+HAVING COUNT(NhanVienKyNang.MANV) >= 3
+
+-- d) Cho biết TenCN có nhiều nhân viên nhất.
+-- GOM SỐ LƯỢNG NHÂN VIÊN THEO TỪNG CHI NHÁNH
+SELECT TenCN, COUNT(NhanVien.MANV) AS SoLuongNhanVien
+FROM ChiNhanh, NhanVien
+WHERE ChiNhanh.MSCN = NhanVien.MSCN
+GROUP BY TenCN
+HAVING COUNT(NhanVien.MANV) >= ALL(
+		SELECT COUNT(NhanVien.MANV)
+		FROM ChiNhanh, NhanVien
+		WHERE ChiNhanh.MSCN = NhanVien.MSCN
+		GROUP BY ChiNhanh.TenCN
+	)
+
+-- e) Cho biết TenCN có ít nhân viên nhất.
+SELECT TenCN, COUNT(NhanVien.MANV) AS SoLuongNhanVien
+FROM ChiNhanh, NhanVien
+WHERE ChiNhanh.MSCN = NhanVien.MSCN
+GROUP BY TenCN
+HAVING COUNT(NhanVien.MANV) <= ALL(
+		SELECT COUNT(NhanVien.MANV)
+		FROM ChiNhanh, NhanVien
+		WHERE ChiNhanh.MSCN = NhanVien.MSCN
+		GROUP BY ChiNhanh.TenCN
+	)
+
+-- f) Với mỗi nhân viên, hãy cho biết số kỹ năng tin học mà nhân viên đó sử dụng được.
+SELECT NhanVien.MANV, Ho, Ten, COUNT(NhanVienKyNang.MSKN) AS SoLuongKyNangSuDung
 FROM NhanVien, NhanVienKyNang
 WHERE NhanVien.MANV = NhanVienKyNang.MANV
-GROUP BY NhanVien.MANV, Ho, Ten, Ngaysinh
-HAVING COUNT(MSKN) >= 3
+GROUP BY NhanVien.MANV, Ho, Ten
+ORDER BY NhanVien.MANV
+
+-- g) Cho biết HoTen, TenCN của nhân viên biết sử dụng nhiều kỹ năng nhất.
+SELECT NhanVien.MANV, Ho, Ten, COUNT(NhanVienKyNang.MSKN) AS SoLuongKyNangSuDung
+FROM NhanVien, NhanVienKyNang
+WHERE NhanVien.MANV = NhanVienKyNang.MANV
+GROUP BY NhanVien.MANV, Ho, Ten
+HAVING COUNT(NhanVienKyNang.MSKN) >= ALL (
+		SELECT COUNT(NhanVienKyNang.MSKN)
+		FROM NhanVien, NhanVienKyNang
+		WHERE NhanVien.MANV = NhanVienKyNang.MANV
+		GROUP BY NhanVien.MANV
+	)
