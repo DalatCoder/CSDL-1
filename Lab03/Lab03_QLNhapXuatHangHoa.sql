f30f199523a8996363a66fd03e860b9dfff4c287
DalatCoder <hieuntctk42@gmail.com>
2020-05-13 21:50:21 +0700

    Bo sung du lieu

--- Lab03/Lab03_QLNhapXuatHangHoa.sql
+++ Lab03/Lab03_QLNhapXuatHangHoa.sql
@@ -7,190 +7,125 @@
 */
 ------------------------------------------------
 
-CREATE DATABASE Lab03_QuanLyNhapXuatHangHoa
-GO
+CREATE DATABASE Lab03_QuanLyNhapXuatHangHoa;
+USE Lab03_QuanLyNhapXuatHangHoa;
 
-USE Lab03_QuanLyNhapXuatHangHoa
-GO
+CREATE TABLE HangHoa
+(
+	id CHAR(5) PRIMARY KEY,
+	ten NVARCHAR(100),
+	dvt NVARCHAR(30),
+	soLuongTon INT
+);
 
-CREATE TABLE HANGHOA (
-	MaHH		char(5) primary key,
-	TenHH		nvarchar(50),
-	DVT			nvarchar(10),
-	SoLuongTon	int check (SoLuongTon >= 0)
-)
-GO
+CREATE TABLE DoiTac
+(
+	id CHAR(5) PRIMARY KEY,
+	ten NVARCHAR(100),
+	diaChi NVARCHAR(100),
+	dienThoai NVARCHAR(100)
+);
 
-CREATE TABLE DOITAC (
-	MaDT		char(5) primary key,
-	TenDT		nvarchar(30),
-	DiaChi		nvarchar(50),
-	DienThoai	char(11)
-)
-GO
+CREATE TABLE KhaNang_CC
+(
+	hangHoa_id CHAR(5),
+	doiTac_id CHAR(5),
+	FOREIGN KEY (hangHoa_id) REFERENCES HangHoa(id),
+	FOREIGN KEY (doiTac_id) REFERENCES DoiTac(id),
+	PRIMARY KEY (hangHoa_id, doiTac_id)
+);
 
-CREATE TABLE KHANANGCC (
-	MaDT	char(5) references DOITAC(MaDT),
-	MaHH	char(5) references HANGHOA(MaHH),
-	primary key(MaDT, MaHH)
-) 
-GO
+CREATE TABLE HoaDon
+(
+	id CHAR(5) PRIMARY KEY,
+	ngayLap DATE,
+	doiTac_id CHAR(5),
+	tongTG FLOAT,
+	FOREIGN KEY (doiTac_id) REFERENCES DoiTac(id) ON DELETE CASCADE
+);
 
-CREATE TABLE HOADON (
-	SoHD	char(5) primary key,
-	NgayLapHD datetime,
-	MaDT	char(5) references DOITAC(MaDT),
-	TongTG	float check (TongTG >= 0)
-)
-GO
+CREATE TABLE CT_HoaDon
+(
+	hoaDon_id CHAR(5),
+	hangHoa_id CHAR(5),
+	donGia INT,
+	soLuong INT,
+	FOREIGN KEY (hoaDon_id) REFERENCES HoaDon(id),
+	FOREIGN KEY (hangHoa_id) REFERENCES HangHoa(id),
+	PRIMARY KEY (hoaDon_id, hangHoa_id)
+);
 
-CREATE TABLE CT_HOADON (
-	SoHD	char(5) references HOADON(SoHD),
-	MaHH	char(5) references HANGHOA(MaHH),
-	DonGia	int check (DonGia > 0),
-	SoLuong int check (SoLuong > 0)
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('CPU01', 'CPU INTEL,CELERON 600 BOX', 'CÁI', 5);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('CPU02', 'CPU INTEL,PIII 700', 'CÁI', 10);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('CPU03', 'CPU AMD K7 ATHL,ON 600', 'CÁI', 8);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('HDD01', 'HDD 10.2 GB QUANTUM', 'CÁI', 10);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('HDD02', 'HDD 13.6 GB SEAGATE', 'CÁI', 15);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('HDD03', 'HDD 20 GB QUANTUM', 'CÁI', 6);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('KB01', 'KB GENIUS', 'CÁI', 12);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('KB02', 'KB MITSUMIMI', 'CÁI', 5);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MB01', 'GIGABYTE CHIPSET INTEL', 'CÁI', 10);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MB02', 'ACOPR BX CHIPSET VIA', 'CÁI', 10);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MB03', 'INTEL PHI CHIPSET INTEL', 'CÁI', 10);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MB04', 'ECS CHIPSET SIS', 'CÁI', 10);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MB05', 'ECS CHIPSET VIA', 'CÁI', 10);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MNT01', 'SAMSUNG 14" SYNCMASTER', 'CÁI', 5);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MNT02', 'LG 14"', 'CÁI', 5);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MNT03', 'ACER 14"', 'CÁI', 8);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MNT04', 'PHILIPS 14"', 'CÁI', 6);
+INSERT INTO HangHoa(id, ten, dvt, soLuongTon) VALUES ('MNT05', 'VIEWSONIC 14"', 'CÁI', 7);
+SELECT * FROM HangHoa;
 
-	primary key (SoHD, MaHH)
-)
-GO
+INSERT INTO DoiTac(id, ten, diaChi, dienThoai) VALUES ('CC001', N'Cty TNC', N'176 BTX Q1 - TPHCM', '08.8250259');
+INSERT INTO DoiTac(id, ten, diaChi, dienThoai) VALUES ('CC002', N'Cty Hoàng Long', N'15A TTT Q1 – TP. HCM', '08.8250898');
+INSERT INTO DoiTac(id, ten, diaChi, dienThoai) VALUES ('CC003', N'Cty Hợp Nhất', N'152 BTX Q1 – TP.HCM', '08.8252376');
+INSERT INTO DoiTac(id, ten, diaChi, dienThoai) VALUES ('K0001', N'Nguyễn Minh Hải', N'91 Nguyễn Văn Trỗi Tp. Đà Lạt', '063.831129');
+INSERT INTO DoiTac(id, ten, diaChi, dienThoai) VALUES ('K0002', N'Như Quỳnh', N'21 Điện Biên Phủ. N.Trang', '058.590270');
+INSERT INTO DoiTac(id, ten, diaChi, dienThoai) VALUES ('K0003', N'Trần nhật Duật', N'Lê Lợi TP. Huế', '054.848376');
+INSERT INTO DoiTac(id, ten, diaChi, dienThoai) VALUES ('K0004', N'Phan Nguyễn Hùng Anh', N'11 Nam Kỳ Khởi nghĩa- TP. Đà lạt', '063.823409');
+SELECT * FROM DoiTac;
 
-INSERT INTO HangHoa VALUES('CPU01', N'CPU INTEL,CELERON 600 BOX' ,N'CÁI', 5)
-INSERT INTO HangHoa VALUES('CPU02', N'CPU INTEL,PIII 700' ,N'CÁI', 10)
-INSERT INTO HangHoa VALUES('CPU03', N'CPU AMD K7 ATHL,ON 600' ,N'CÁI', 8)
-INSERT INTO HangHoa VALUES('HDD01', N'HDD 10.2 GB QUANTUM' ,N'CÁI', 10)
-INSERT INTO HangHoa VALUES('HDD02', N'HDD 13.6 GB SEAGATE' ,N'CÁI', 15)
-INSERT INTO HangHoa VALUES('HDD03', N'HDD 20 GB QUANTUM' ,N'CÁI', 6)
-INSERT INTO HangHoa VALUES('KB01', N'KB GENIUS' ,N'CÁI', 12)
-INSERT INTO HangHoa VALUES('KB02', N'KB MITSUMIMI' ,N'CÁI', 5)
-INSERT INTO HangHoa VALUES('MB01', N'GIGABYTE CHIPSET INTEL' ,N'CÁI', 10)
-INSERT INTO HangHoa VALUES('MB02', N'ACOPR BX CHIPSET VIA' ,N'CÁI', 10)
-INSERT INTO HangHoa VALUES('MB03', N'INTEL PHI CHIPSET INTEL' ,N'CÁI', 10)
-INSERT INTO HangHoa VALUES('MB04', N'ECS CHIPSET SIS' ,N'CÁI', 10)
-INSERT INTO HangHoa VALUES('MB05', N'ECS CHIPSET VIA' ,N'CÁI', 10)
-INSERT INTO HangHoa VALUES('MNT01', N'SAMSUNG 14" SYNCMASTER' ,N'CÁI', 5)
-INSERT INTO HangHoa VALUES('MNT02', N'LG 14"' ,N'CÁI', 5)
-INSERT INTO HangHoa VALUES('MNT03', N'ACER 14"' ,N'CÁI', 8)
-INSERT INTO HangHoa VALUES('MNT04', N'PHILIPS 14"' ,N'CÁI', 6)
-INSERT INTO HangHoa VALUES('MNT05', N'VIEWSONIC 14"' ,N'CÁI', 7)
+SET DATEFORMAT dmy;
+INSERT INTO HoaDon(id, ngayLap, doiTac_id) VALUES('N0001', '25/01/2006', 'CC001');
+INSERT INTO HoaDon(id, ngayLap, doiTac_id) VALUES('N0002', '01/05/2006', 'CC002');
+INSERT INTO HoaDon(id, ngayLap, doiTac_id) VALUES('X0001', '12/05/2006', 'K0001');
+INSERT INTO HoaDon(id, ngayLap, doiTac_id) VALUES('X0002', '16/06/2006', 'K0002');
+INSERT INTO HoaDon(id, ngayLap, doiTac_id) VALUES('X0003', '20/04/2006', 'K0001');
+SELECT * FROM HoaDon;
 
-SELECT * FROM HANGHOA
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC001', 'CPU01');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC001', 'HDD03');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC001', 'KB01');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC001', 'MB02');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC001', 'MB04');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC001', 'MNT01');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC002', 'CPU01');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC002', 'CPU02');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC002', 'CPU03');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC002', 'KB02');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC002', 'MB01');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC002', 'MB05');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC002', 'MNT03');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC003', 'HDD01');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC003', 'HDD02');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC003', 'HDD03');
+INSERT INTO KhaNang_CC(doiTac_id, hangHoa_id) VALUES ('CC003', 'MB03');
+SELECT * FROM KhaNang_CC;
 
-INSERT INTO DoiTac VALUES ('CC001', N'Cty TNC', N'176 BTX Q1 - TPHCM' ,'08.8250259')
-INSERT INTO DoiTac VALUES ('CC002', N'Cty Hoàng Long', N'15A TTT Q1 – TP. HCM' ,'08.8250898')
-INSERT INTO DoiTac VALUES ('CC003', N'Cty Hợp Nhất', N'152 BTX Q1 – TP.HCM' ,'08.8252376')
-INSERT INTO DoiTac VALUES ('K0001', N'Nguyễn Minh Hải', N'91 Nguyễn Văn Trỗi Tp. Đà Lạt' ,'063.831129')
-INSERT INTO DoiTac VALUES ('K0002', N'Như Quỳnh', N'21 Điện Biên Phủ. N.Trang' ,'058590270')
-INSERT INTO DoiTac VALUES ('K0003', N'Trần nhật Duật', N'Lê Lợi TP. Huế' ,'054.848376')
-INSERT INTO DoiTac VALUES ('K0004', N'Phan Nguyễn Hùng Anh', N'11 Nam Kỳ Khởi nghĩa- TP. Đà lạt' ,'063.823409')
-
-SELECT * FROM DOITAC
-
-SET DATEFORMAT DMY 
-GO
-INSERT INTO HoaDon VALUES('N0001', '25/01/2006' ,'CC001', NULL)
-INSERT INTO HoaDon VALUES('N0002', '01/05/2006' ,'CC002', NULL)
-INSERT INTO HoaDon VALUES('X0001', '12/05/2006' ,'K0001', NULL)
-INSERT INTO HoaDon VALUES('X0002', '16/06/2006' ,'K0002', NULL)
-INSERT INTO HoaDon VALUES('X0003', '20/04/2006' ,'K0001', NULL)
-
-SELECT * FROM HOADON
-
-INSERT INTO KHANANGCC VALUES ('CC001', 'CPU01')
-INSERT INTO KHANANGCC VALUES ('CC001', 'HDD03')
-INSERT INTO KHANANGCC VALUES ('CC001', 'KB01')
-INSERT INTO KHANANGCC VALUES ('CC001', 'MB02')
-INSERT INTO KHANANGCC VALUES ('CC001', 'MB04')
-INSERT INTO KHANANGCC VALUES ('CC001', 'MNT01')
-INSERT INTO KHANANGCC VALUES ('CC002', 'CPU01')
-INSERT INTO KHANANGCC VALUES ('CC002', 'CPU02')
-INSERT INTO KHANANGCC VALUES ('CC002', 'CPU03')
-INSERT INTO KHANANGCC VALUES ('CC002', 'KB02')
-INSERT INTO KHANANGCC VALUES ('CC002', 'MB01')
-INSERT INTO KHANANGCC VALUES ('CC002', 'MB05')
-INSERT INTO KHANANGCC VALUES ('CC002', 'MNT03')
-INSERT INTO KHANANGCC VALUES ('CC003', 'HDD01')
-INSERT INTO KHANANGCC VALUES ('CC003', 'HDD02')
-INSERT INTO KHANANGCC VALUES ('CC003', 'HDD03')
-INSERT INTO KHANANGCC VALUES ('CC003', 'MB03')
-
-SELECT * FROM KHANANGCC
-
-INSERT INTO CT_HOADON VALUES ('N0001', 'CPU01' ,63 ,10)
-INSERT INTO CT_HOADON VALUES ('N0001', 'HDD03' ,97 ,7)
-INSERT INTO CT_HOADON VALUES ('N0001', 'KB01' ,3 ,5)
-INSERT INTO CT_HOADON VALUES ('N0001', 'MB02' ,57 ,5)
-INSERT INTO CT_HOADON VALUES ('N0001', 'MNT01' ,112 ,3)
-INSERT INTO CT_HOADON VALUES ('N0002', 'CPU02' ,115 ,3)
-INSERT INTO CT_HOADON VALUES ('N0002', 'KB02' ,5 ,7)
-INSERT INTO CT_HOADON VALUES ('N0002', 'MNT03' ,111 ,5)
-INSERT INTO CT_HOADON VALUES ('X0001', 'CPU01' ,67 ,2)
-INSERT INTO CT_HOADON VALUES ('X0001', 'HDD03' ,100 ,2)
-INSERT INTO CT_HOADON VALUES ('X0001', 'KB01' ,5 ,2)
-INSERT INTO CT_HOADON VALUES ('X0001', 'MB02' ,62 ,1)
-INSERT INTO CT_HOADON VALUES ('X0002', 'CPU01' ,67 ,1)
-INSERT INTO CT_HOADON VALUES ('X0002', 'KB02' ,7 ,3)
-INSERT INTO CT_HOADON VALUES ('X0002', 'MNT01' ,115 ,2)
-INSERT INTO CT_HOADON VALUES ('X0003', 'CPU01' ,67 ,1)
-INSERT INTO CT_HOADON VALUES ('X0003', 'MNT03' ,115 ,2)
-
-SELECT * FROM CT_HOADON
-
---1) Liệt kê các mặt hàng thuộc loại đĩa cứng.
-SELECT *
-FROM HANGHOA
-WHERE MaHH LIKE 'HDD%'
-
---2) Liệt kê các mặt hàng có số lượng tồn trên 10.
-SELECT *
-FROM HANGHOA
-WHERE HANGHOA.SoLuongTon > 10
-
---3) Cho biết thông tin về các nhà cung cấp ở Thành phố Hồ Chí Minh
-SELECT *
-FROM DOITAC
-WHERE MaDT LIKE 'CC%'
-
---4) Liệt kê các hóa đơn nhập hàng trong tháng 5/2006, thông tin hiển thị gồm: 
--- sohd;ngaylaphd; tên, địa chỉ, và điện thoại của nhà cung cấp; số mặt hàng
-
-
---5) Cho biết tên các nhà cung cấp có cung cấp đĩa cứng.
-SELECT DOITAC.MaDT, TenDT, HANGHOA.TenHH
-FROM HANGHOA, KHANANGCC, DOITAC
-WHERE 
-	HANGHOA.MaHH = KHANANGCC.MaHH AND KHANANGCC.MaDT = DOITAC.MaDT AND
-	HANGHOA.TenHH LIKE 'HDD%' AND DOITAC.MaDT LIKE 'CC%'
-
---6) Cho biết tên các nhà cung cấp có thể cung cấp tất cả các loại đĩa cứng.
--- NHÓM XEM CÓ BAO NHIEU LOẠI ĐĨA CỨNG
-SELECT COUNT(HANGHOA.MaHH) AS SoLoai
-FROM HANGHOA
-WHERE HANGHOA.MaHH LIKE 'HDD%'
-
--- ĐẾM SỐ LƯỢNG ĐĨA CỨNG CÁC NHÀ CUNG CẤP
-SELECT DOITAC.MaDT, TenDT, DiaChi
-FROM DOITAC, KHANANGCC
-WHERE DOITAC.MaDT = KHANANGCC.MaDT AND KHANANGCC.MaHH LIKE 'HDD%'
-GROUP BY DOITAC.MaDT, TenDT, DiaChi
-HAVING COUNT(DOITAC.MaDT) =	(SELECT COUNT(HANGHOA.MaHH) AS SoLoai
-							 FROM HANGHOA
-							 WHERE HANGHOA.MaHH LIKE 'HDD%')
-
---7) Cho biết tên nhà cung cấp không cung cấp đĩa cứng.
--- Tìm đối tác cung cấp đĩa cứng
-SELECT *
-FROM DOITAC, KHANANGCC
-WHERE DOITAC.MaDT = KHANANGCC.MaDT AND KHANANGCC.MaHH LIKE 'HDD%'
-
--- Toàn bộ đối tác ngoại trừ những người cung cấp đĩa cứng
-SELECT *
-FROM DOITAC
-WHERE DOITAC.MaDT LIKE 'CC%' AND DOITAC.MaDT NOT IN (SELECT DOITAC.MaDT
-						  FROM DOITAC, KHANANGCC				
-						  WHERE 
-							DOITAC.MaDT = KHANANGCC.MaDT AND 
-							KHANANGCC.MaHH LIKE 'HDD%' AND 
-							DOITAC.MaDT LIKE 'CC%'
-						  )
-
---8: Cho biết thông tin của mặt chưa bán được.
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0001', 'CPU01', 63, 10);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0001', 'HDD03', 97, 7);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0001', 'KB01', 3, 5);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0001', 'MB02', 57, 5);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0001', 'MNT01', 112, 3);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0002', 'CPU02', 115, 3);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0002', 'KB02', 5, 7);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('N0002', 'MNT03', 111, 5);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0001', 'CPU01', 67, 2);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0001', 'HDD03', 100, 2);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0001', 'KB01', 5, 2);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0001', 'MB02', 62, 1);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0002', 'CPU01', 67, 1);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0002', 'KB02', 7, 3);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0002', 'MNT01', 115, 2);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0003', 'CPU01', 67, 1);
+INSERT INTO CT_HoaDon(hoaDon_id, hangHoa_id, donGia, soLuong) VALUES ('X0003', 'MNT03', 115, 2);
+SELECT * FROM CT_HoaDon;
