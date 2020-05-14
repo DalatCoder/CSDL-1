3736f5efcf0081001fdef931e5412efe41381b67
DalatCoder <hieuntctk42@gmail.com>
2020-05-14 20:47:20 +0700

    Hoan thanh lab04 Quan ly dat bao

--- Lab04/Lab04_QuanLyDatBao.sql
+++ Lab04/Lab04_QuanLyDatBao.sql
@@ -83,20 +83,110 @@ SELECT * FROM DatBao;
 
 
 -- 1) Cho biết các tờ báo, tạp chí (MABAOTC, TEN, GIABAN) có định kỳ phát hành hàng tuần (Tuần báo).
+SELECT id AS MaBao, ten AS Ten, giaBan
+FROM Bao
+WHERE dinhKy = N'Tuần báo';
+
 -- 2) Cho biết thông tin về các tờ báo thuộc loại báo phụ nữ (mã báo tạp chí bắt đầu bằng PN).
--- 3) Cho biết tên các khách hàng có đặt mua báo phụ nữ (mã báo tạp chí bắt đầu bằng PN), không liệt kê khách hàng trùng.
+SELECT * FROM Bao
+WHERE id LIKE 'PN%';
+
+-- 3) Cho biết tên các khách hàng có đặt mua báo phụ nữ (mã báo tạp chí bắt đầu bằng PN), 
+-- không liệt kê khách hàng trùng.
+SELECT DISTINCT khachHang_id AS MaKH, ten, diaChi FROM DatBao
+JOIN KhachHang ON KhachHang.id = DatBao.khachHang_id
+WHERE bao_id LIKE 'PN%';
+
 -- 4) Cho biết tên các khách hàng có đặt mua tất cả các báo phụ nữ (mã báo tạp chí bắt đầu bằng PN).
+SELECT KhachHang.id AS MaKH, ten, diaChi 
+FROM DatBao
+JOIN KhachHang ON KhachHang.id = DatBao.khachHang_id
+WHERE bao_id LIKE 'PN%'
+GROUP BY KhachHang.id, ten, diaChi
+HAVING COUNT(DISTINCT DatBao.bao_id) = (
+SELECT COUNT(*)
+FROM Bao
+WHERE id LIKE 'PN%');
+
 -- 5) Cho biết các khách hàng không đặt mua báo thanh niên.
+SELECT *
+FROM KhachHang
+WHERE id NOT IN(
+SELECT khachHang_id
+FROM DatBao
+WHERE bao_id LIKE 'TN%');
+
 -- 6) Cho biết số tờ báo mà mỗi khách hàng đã đặt mua.
+SELECT KhachHang.id AS MaKH, ten, diaChi, SUM(soLuongMua) AS TongSoLuongMua
+FROM DatBao
+JOIN KhachHang ON KhachHang.id = DatBao.khachHang_id
+GROUP BY KhachHang.id, ten, diaChi;
+
 -- 7) Cho biết số khách hàng đặt mua báo trong năm 2004.
+SELECT COUNT(DISTINCT khachHang_id) AS SoKhachHang
+FROM DatBao
+WHERE YEAR(ngayDatMua) = 2004;
+
 -- 8) Cho biết thông tin đặt mua báo của các khách hàng 
--- (TenKH, TeN, DinhKy,
--- SLMua, SoTien), trong đó SoTien = SLMua  DonGia.
+-- (TenKH, TeN, DinhKy, SLMua, SoTien), trong đó SoTien = SLMua x DonGia.
+SELECT KhachHang.id AS MaKH, KhachHang.ten AS TenKH, KhachHang.diaChi, Bao.ten AS TenBao, Bao.dinhKy, SUM(soLuong) AS TongSoLuongMua, SUM(soLuongMua * giaBan) AS ThanhTien
+FROM KhachHang
+JOIN DatBao ON KhachHang.id = DatBao.khachHang_id
+JOIN Bao ON Bao.id = DatBao.bao_id
+GROUP BY KhachHang.id, KhachHang.ten, KhachHang.diaChi, Bao.id, Bao.ten, Bao.dinhKy;
+
 -- 9) Cho biết các tờ báo, tạp chí (Ten, DinhKy) và tổng số lượng đặt mua của các
 -- khách hàng đối với tờ báo, tạp chí đó.
+SELECT Bao.id AS MaBao, Bao.ten AS Ten, Bao.dinhKy, SUM(soLuongMua) AS TongSoLuongMua
+FROM DatBao
+JOIN Bao ON Bao.id = DatBao.bao_id
+GROUP BY Bao.id, Bao.ten, Bao.dinhKy
+ORDER BY TongSoLuongMua DESC;
+
 -- 10) Cho biết tên các tờ báo dành cho học sinh, sinh viên (mã báo tạp chí bắt đầu bằng HS).
+SELECT *
+FROM Bao
+WHERE id LIKE 'HS%';
+
 -- 11) Cho biết những tờ báo không có người đặt mua.
+SELECT *
+FROM Bao
+WHERE id NOT IN(
+SELECT DISTINCT bao_id
+FROM DatBao);
+
 -- 12) Cho biết tên, định kỳ của những tờ báo có nhiều người đặt mua nhất.
+SELECT Bao.id AS MaBao, Bao.ten AS Ten, Bao.dinhKy, SUM(soLuongMua) AS TongSoLuongMua
+FROM DatBao
+JOIN Bao ON Bao.id = DatBao.bao_id
+GROUP BY Bao.id, Bao.ten, Bao.dinhKy
+HAVING SUM(soLuongMua) >= ALL (
+SELECT SUM(soLuongMua)
+FROM DatBao
+JOIN Bao ON Bao.id = DatBao.bao_id
+GROUP BY Bao.id, Bao.dinhKy
+);
+
 -- 13) Cho biết khách hàng đặt mua nhiều báo, tạp chí nhất.
+SELECT KhachHang.id AS MaKH, KhachHang.ten, KhachHang.diaChi, SUM(soLuongMua) AS TongSoLuongMua
+FROM KhachHang
+JOIN DatBao ON KhachHang.id = DatBao.khachHang_id
+GROUP BY KhachHang.id, KhachHang.ten, KhachHang.diaChi
+HAVING SUM(soLuongMua) >= ALL(
+SELECT SUM(soLuongMua)
+FROM DatBao
+GROUP BY khachHang_id
+);
+
 -- 14) Cho biết các tờ báo phát hành định kỳ một tháng 2 lần.
+SELECT *
+FROM Bao
+WHERE dinhKy = N'Bán nguyệt san';
+
 -- 15) Cho biết các tờ báo, tạp chi có từ 3 khách hàng đặt mua trở lên.
+SELECT Bao.id AS MaBao, Bao.ten, Bao.dinhKy, COUNT(DISTINCT khachHang_id) AS SoLuongKhachMua
+FROM DatBao
+JOIN Bao ON Bao.id = DatBao.bao_id
+GROUP BY Bao.id, Bao.ten, Bao.dinhKy
+HAVING COUNT(DISTINCT khachHang_id) >= 3;
+
